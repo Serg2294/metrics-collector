c69e81532d7b2185bc54925231691af5c58408fb
Serg2294 <serg220394@gmail.com>
2018-05-15 17:17:36 +0300

    Changed value from int to percent

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 import psutil
 import sys
 if sys.argv[1] == "cpu":
-        print('system.cpu.idle {0:.1f}'.format(psutil.cpu_times().idle))
-        print('system.cpu.user {0:.1f}'.format(psutil.cpu_times().user))
-        print('system.cpu.guest {0:.1f}'.format(psutil.cpu_times().guest))
-        print('system.cpu.iowait {0:.1f}'.format(psutil.cpu_times().iowait))
-        print('system.cpu.steal {0:.1f}'.format(psutil.cpu_times().steal))
-        print('system.cpu.system {0:.1f}'.format(psutil.cpu_times().system))
+        print('system.cpu.idle {0:.1f}'.format(psutil.cpu_times_percent(interval=1).idle))
+        print('system.cpu.user {0:.1f}'.format(psutil.cpu_times_percent(interval=1).user))
+        print('system.cpu.guest {0:.1f}'.format(psutil.cpu_times_percent(interval=1).guest))
+        print('system.cpu.iowait {0:.1f}'.format(psutil.cpu_times_percent(interval=1).iowait))
+        print('system.cpu.steal {0:.1f}'.format(psutil.cpu_times_percent(interval=1).steal))
+        print('system.cpu.system {0:.1f}'.format(psutil.cpu_times_percent(interval=1).system))
 elif sys.argv[1] == "mem":
         print('virtual total {0}'.format(psutil.virtual_memory().total))
         print('virtual used {0}'.format(psutil.virtual_memory().used))
@@ -16,4 +16,4 @@ elif sys.argv[1] == "mem":
         print('swap used {0}'.format(psutil.swap_memory().used))
         print('swap free {0}'.format(psutil.swap_memory().free))
 else:
-        print('Wrong argument. Please write cpu or mem.')
+        print('Wrong argumet. Please write cpu or mem.')
